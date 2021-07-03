04013167fec2f518506069b229ecc2523b963098
Nicolas Silva dos Santos <nicolas.ssantos@stone.com.br>
2021-07-03 16:04:53 -0300

    feat: firs exemple of extract function

---
+++ MartinFowler.Refactoring.Examples/Example01.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MartinFowler.Refactoring.Examples
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Performance
+    {
+        public string PlayId { get; set; }
+        public int Audience { get; set; }
+
+        public Performance(string playId, int audience)
+        {
+            PlayId = playId;
+            Audience = audience;
+        }
+    }
+
+    public class Invoice
+    {
+        public Customer Customer { get; set; }
+        public List<Performance> Performances { get; set; }
+
+        public Invoice(Customer customer, List<Performance> performances)
+        {
+            Customer = customer;
+            Performances = performances;
+        }
+    }
+
+    public class Play
+    {
+        public Play(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Example01
+    {
+
+        public string Statement(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            decimal totalAmount = 0;
+            double volumeCredits = 0;
+
+            var result = $"Statement for {invoice.Customer}\n";
+
+            foreach (var performance in invoice.Performances)
+            {
+                var play = plays[performance.PlayId];
+
+                decimal amount = 0;
+
+                // partner name: extract function
+                amount = AmountFor(performance, play);
+
+                // add volume credits
+                volumeCredits += Math.Max(performance.Audience - 30, 0);
+
+                // add extra credit for every ten comedy attendees
+                if (play.Type == "comedy")
+                {
+                    volumeCredits += Math.Floor(Convert.ToDouble(performance.Audience / 5));
+                }
+
+                // print line for this order
+                result += $"  {play.Name}: {(amount / 100).ToString("C")} ({performance.Audience} seats)\n";
+                totalAmount += amount;
+            }
+
+            result += $"Amount owed is {(totalAmount / 100).ToString("C")}\n";
+            result += $"You earned {volumeCredits} credits";
+
+            return result;
+        }
+
+        private decimal AmountFor(Performance performance, Play play)
+        {
+            decimal amount;
+            
+            switch (play.Type)
+            {
+                case "tragedy":
+                    amount = 40000;
+                    if (performance.Audience > 30)
+                    {
+                        amount += 1000 * (performance.Audience - 30);
+                    }
+                    break;
+                case "comedy":
+                    amount = 30000;
+                    if (performance.Audience > 20)
+                    {
+                        amount += 10000 + 500 * (performance.Audience - 20);
+                    }
+                    amount += 300 * performance.Audience;
+                    break;
+                default:
+                    throw new Exception($"unknow type for: {play.Type}");
+            }
+
+            return amount;
+        }
+    }
+}
---
+++ MartinFowler.Refactoring.Tests/Example01Test.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using MartinFowler.Refactoring.Examples;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MartinFowler.Refactoring.Tests
+{
+    public class Example01Test
+    {
+        [Fact]
+        public void GivenInvoiceWithAListdePerformances_WhenExecuteStatement_ShouldReturnCalculateAInvoiceResult()
+        {
+            var example01 = new Example01();
+
+            var customer = new Customer() { Name = "BigCo" };
+
+            var plays = new Dictionary<string, Play>()
+            {
+                {
+                    "hamlet", new Play("Hamlet","tragedy")
+                },
+                {
+                    "as-like", new Play("As You Like It","comedy")
+                },
+                {
+                    "othello", new Play("Othello","tragedy")
+                }
+            };
+
+            var invoice = new Invoice(customer, new List<Performance> { 
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)
+            });
+            
+            var result = example01.Statement(
+                invoice,
+                plays);
+
+            var expected = "Statement for BigCo\n";
+            expected += "  Hamlet: R$ 650,00 (55 seats)\n";
+            expected += "  As You Like It: R$ 580,00 (35 seats)\n";
+            expected += "  Othello: R$ 500,00 (40 seats)\n";
+            expected += "Amount owed is R$ 1.730,00\n";
+            expected += "You earned 47 credits";
+
+            result.Should().Be(expected);
+        }
+    }
+}
